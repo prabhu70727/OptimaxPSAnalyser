c76f77a9869f83191decf992f250f667c5a39872
Prabhakaran Santhanam <psanthanam@ethz.ch>
2021-09-03 07:14:10 +0530

    Initial commit

---
+++ src/ch/ethz/optimax/analyser/Main.java
@@ -0,0 +1,30 @@
+package ch.ethz.optimax.analyser;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.filefilter.DirectoryFileFilter;
+import org.apache.commons.io.filefilter.RegexFileFilter;
+
+import java.io.File;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Iterator;
+
+public class Main {
+
+    public static void main(String[] args) {
+        HashMap dateCountHashMap = new HashMap <String, HashMap<String, HashMap<String, Integer>>> ();
+
+        Collection files = FileUtils.listFiles(
+                new File("./"),
+                new RegexFileFilter("^.*\\.xml$"),
+                DirectoryFileFilter.DIRECTORY
+        );
+
+        Iterator iterator = files.iterator();
+
+        while (iterator.hasNext()) {
+            System.out.println(iterator.next());
+        }
+
+    }
+}
